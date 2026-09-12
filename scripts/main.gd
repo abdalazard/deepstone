eda3c7ced29c53b81f7bfc8e53c55448e1fa6913
Vinícius Abdala <abdalazard@gmail.com>
2026-09-12 05:45:40 -0300

    Add indestructible concrete platform for the chest at the surface

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const ROCK_SCENE = preload("res://scenes/cave/rock.tscn")
 const DIRT_SCENE = preload("res://scenes/cave/dirt.tscn")
+const CONCRETE_SCENE = preload("res://scenes/cave/concrete.tscn")
 const TORCH_SCENE = preload("res://scenes/environment/torch.tscn")
 
 func _ready() -> void:
@@ -14,7 +15,11 @@ func generate_world() -> void:
 			var tile_pos = Vector2(x * 16 + 8, y * 16 + 128)
 				
 			var instance
-			if randf() > 0.7:
+			
+			# Create a 3-block wide concrete platform for the chest at the surface (y=0)
+			if y == 0 and x in [14, 15, 16]:
+				instance = CONCRETE_SCENE.instantiate()
+			elif randf() > 0.7:
 				instance = ROCK_SCENE.instantiate()
 				if randf() > 0.8:
 					instance.is_copper = true # It's GOLD now
