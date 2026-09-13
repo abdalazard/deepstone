f17c67df22d17451614e2fafdecbe39448b93ad7
Vinícius Abdala <abdalazard@gmail.com>
2026-09-13 15:04:49 -0300

    fix: pass required ClockDirection argument to Image.rotate_90

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -92,7 +92,7 @@ var column_tex: Texture2D = null
 func _get_column_tex() -> Texture2D:
 	if column_tex == null:
 		var img: Image = brick_tex.get_image()
-		img.rotate_90()
+		img.rotate_90(ClockDirection.CLOCKWISE)
 		column_tex = ImageTexture.create_from_image(img)
 	return column_tex
 
